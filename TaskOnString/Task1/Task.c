b131527297a682f5a477fb5289cb54a3574b73a7
VladimirSdobniv <itmm-2213-a@E.local>
2022-10-13 08:43:12 +0300

    Интерфейс строк

--- TaskOnString/Task1/Task.c
+++ TaskOnString/Task1/Task.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <locale.h>
 int main() {
+	setlocale(LC_ALL, "rus");
 	char* str;
+	int flag = 1, id;
 	str = (char*)malloc(sizeof(char) * 100);
 
+	printf("Введите строку (до 100 символов): ");
 	gets(str);
-	str[6] = '\0';
-	puts(str);
-
-
-	int size = 0;
+    int size = 0;
 	while (str[size] != '\0') {
 		size++;
+	}
+	printf("%d", size);
+	while (flag) {
+		printf("Выберите действие над строкой:\n1) Заменить строку\n2) Обрезать строку\n3) Развернуть строку \n4) Сменить регистр\n0) Выход");
+		scanf_s("%d", &id);
+
 	}
 
 	free(str);
